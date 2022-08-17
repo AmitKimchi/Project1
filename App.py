17d2686a3f950db750acb954d43137d39c0149e8
Amit <amitk875@gmail.com>
2022-08-17 14:16:21 +0300

    Practice2222

---
+++ App.py
@@ -0,0 +1,55 @@
+
+from random import randint
+num = randint(1,9)
+guessnum=int(input("Enter the selected number: "))
+print(num)
+while num!= guessnum:
+    if guessnum < num:
+        print("Higher")
+        print(num)
+    else:
+        print("Lower")
+        print(num)
+    guessnum = int(input("Enter the selected number: "))
+print("You did it you fucking ni**er")
+print(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
